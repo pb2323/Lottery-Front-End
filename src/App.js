72ec7666754b9aa2b51ed688643797858c405e42
pb2323 <pbajaj0023@gmail.com>
2021-03-09 15:51:40 +0530

    lottery FE partially done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// function App() {
+//   const web3 = new Web3(window.web3.currentProvider);
+//   window.ethereum.request({ method: "eth_requestAccounts" }).then((res) => {
+//     console.log(res);
+//   });
+
+//   return (
+//     <div className="App">
+//       <h2>Lottery Contract</h2>
+//       <p>This contract is managed by {this.state.manager}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default class App extends Component {
+  state = {
+    manager: "",
+    account: "",
+    balance: "",
+    players: [],
+  };
+  async componentDidMount() {
+    const account = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ account: account[0], manager, players, balance });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This contract is managed by {this.state.manager} .There are currently{" "}
+          {this.state.players.length} players entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
-export default App;
